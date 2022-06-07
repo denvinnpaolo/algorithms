03dc1340c892321c4d047bacdf1dfebad62814b3
Denvinn Magsino <denvinnpaolo@gmail.com>
2022-06-07 16:10:32 -0700

    Time: 449 ms (97.48%), Space: 26.1 MB (29.72%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.py
+++ 217-contains-duplicate/217-contains-duplicate.py
@@ -1,22 +1,23 @@
-# GOAL: Scan the array to see if there are only one value for each element in the array.
-# if a value is repeated return false else true
-# input == list of int
-# output == boolean
-# maximum input = 10^5 (100,000)
-# minimum input = 1
+# int array nums
+# return true if num appears more than once else returns false
+# array not sorted
+# if empty return Null
 
-# element value is from -10^9 <= 10^9
+# steps
+# iterate through the array
+# add value in a set
+# if num in set return True
+# if iteration finishes return False
 
 
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        passedVals = set([])
+        seen = set()
         
-        for i in range(len(nums)):
-            cur_val = nums[i]
-            if cur_val in passedVals:
+        for num in nums:
+            if num not in seen:
+                seen.add(num)
+            else:
                 return True
-            
-            passedVals.add(cur_val)
         
         return False
