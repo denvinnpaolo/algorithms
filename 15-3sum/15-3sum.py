182d713abbf42c19c088e7c8d3e2dd9c573c49d3
Denvinn Magsino <denvinnpaolo@gmail.com>
2022-06-18 13:42:40 -0700

    Time: 3989 ms (19.32%), Space: 17.3 MB (87.74%) - LeetHub

---
+++ 15-3sum/15-3sum.py
@@ -0,0 +1,28 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        if len(nums) < 3:
+            return []
+        
+        nums = sorted(nums)
+        n = len(nums)
+        res = set()
+        for i in range(n - 2):
+            left = i + 1
+            right = n - 1
+            
+
+            
+            while left < right:
+                potNum = nums[i] + nums[left] + nums[right]
+                if potNum == 0:
+                    ans = (nums[i], nums[left], nums[right])
+                    res.add(ans)
+                    right -= 1
+                    left += 1
+                elif  potNum > 0:
+                    right -= 1
+                else:
+                    left += 1
+        
+    
+        return res
