e57992fb826bbc7e7535bab6e0123b473f986203
Denvinn Magsino <denvinnpaolo@gmail.com>
2022-06-21 21:11:46 -0700

    Time: 71 ms (53.91%), Space: 15 MB (23.45%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,34 +1,6 @@
-# given two string input
-# input must be same length
-# inputs are anagrams if they contain same exact number of letters
-# in anagrams, letters can be rearranged
-
-# 2. check if len(s) == len(t)
-# 1. check if s and t are type string
-# 3. choose to iterate and append every char of either s or t to a hash table
-# 4. if string has a letter that isn't in hash table or key == 0 then return False
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False
+        s = sorted(s)
+        t = sorted(t)
         
-        di = {}
-        
-        # add letters to dict
-        for c in s:
-            if c in di:
-                di[c] += 1
-            else:
-                di[c] = 1
-        
-        for c in t:
-            if c in di:
-                di[c] -= 1
-                if di[c] == -1:
-                    return False
-            else:
-                return False
-        return True
-                
-            
-        
+        return True if t == s else False
