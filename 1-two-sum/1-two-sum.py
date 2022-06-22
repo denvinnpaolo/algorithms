6ff44ffebff579a04f79b464b4eeb23052473dd8
Denvinn Magsino <denvinnpaolo@gmail.com>
2022-06-21 21:14:43 -0700

    Time: 106 ms (51.74%), Space: 15.3 MB (24.17%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,29 +1,13 @@
-# int nums
-# given a target
-# must return indices of two numbers that add up to target
-# there's always a solution within the array
-# can't use same element twice but can use the same number
-
-# steps
-# 1. go through each number in nums
-# 2. subtract num to target
-# 3. check if number looking for was passed
-# 4. continue until found
-
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # key: value
-        # num: index
-        d ={}
-        
+        d = dict()
         
         for i in range(len(nums)):
-            pot_num = target - nums[i]
+            num = target - nums[i]
             
-            if pot_num in d:
-                return [d[pot_num], i]
+            if num in d:
+                return [d[num], i]
             else:
                 d[nums[i]] = i
-                
-                
-                
+        
+        print(d)
