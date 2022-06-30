e114ec89d7885cdbfe50d3ec948e1e2357febe3b
Denvinn Magsino <denvinnpaolo@gmail.com>
2022-06-29 18:47:41 -0700

    Time: 1606 ms (60.05%), Space: 25 MB (49.65%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.py
+++ 739-daily-temperatures/739-daily-temperatures.py
@@ -1,21 +1,14 @@
-# temp array denotes temp at a given day
-# temp has int elements
-# 1 <= n <= 10^5
-# Goal: return the result of how many days will it take for the the temp to get warmer from our current position
-
-
-
 class Solution:
     def dailyTemperatures(self, temps: List[int]) -> List[int]:
         res = [0] * len(temps)
+        
         s = []
-        for i, t in enumerate(temps):
-            while s and t > s[-1][0]:
-                stackT, stackIdx = s.pop()
+        
+        for i,t in enumerate(temps):
+            while s and t> s[-1][0]:
+                sTemp, sIdx = s.pop()
                 
-                res[stackIdx] = (i - stackIdx)
+                res[sIdx] = (i - sIdx)
             s.append([t, i])
-            
-            
             
         return res
