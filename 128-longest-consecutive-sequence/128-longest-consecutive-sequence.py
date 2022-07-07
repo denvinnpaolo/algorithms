7eb24d3134b084def7c89ed4eca52e4bcc7c272a
Denvinn Magsino <denvinnpaolo@gmail.com>
2022-07-06 21:55:59 -0700

    Time: 3427 ms (14.19%), Space: 28 MB (61.05%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.py
@@ -8,7 +8,7 @@ class Solution:
             cur = 1
             nxt = num + 1
             
-            if num - 1 not in s and num in s:
+            if num - 1 not in s:
                 while nxt in s:
                     cur += 1
                     nxt += 1
