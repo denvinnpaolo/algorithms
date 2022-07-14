22f99a297f68654cad5e4fdb150770576bbc4e41
Denvinn Magsino <denvinnpaolo@gmail.com>
2022-07-14 10:14:41 -0700

    Time: 97 ms (22.87%), Space: 14.6 MB (34.60%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,20 +1,21 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False
+        d = dict()
         
-        d = {}
-        for c in s:
-            if c in d:
-                d[c] += 1
+        for ch in s:
+            if ch in d:
+                d[ch] += 1
             else:
-                d[c] = 1
+                d[ch] = 1
                 
-        for c in t:
-            if c in d and d[c] != 0:
-                d[c] -= 1
-
-                if d[c] == 0:
-                    del d[c]
-                        
-        return len(d) == 0
+        for ch in t:
+            if ch in d:
+                d[ch] -= 1
+                
+                if d[ch] == 0:
+                    del d[ch]
+            else:
+                d[ch] = 1
+            
+        print(d)
+        return True if len(d) == 0 else False
